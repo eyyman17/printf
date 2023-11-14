a09e1ca51fd81de49c4c54217faa02a67d20b7ab
Rafiki_Mostafa <rafikimostafa@gmail.com>
2023-11-14 14:02:14 -0800

    fix betty error

--- _putchar.c
+++ _putchar.c
@@ -3,7 +3,7 @@
 
 /**
  * _putchar - write the character c to stdout
- * 0c: the character to print
+ * @c: the character to print
  *
  * Return: On success 1.
  *	On error, -1 is returned, and error is set appropriately
