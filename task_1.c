829c7fc23e57b3dbb5d6d59a5671e9573e143d26
Rafiki_Mostafa <rafikimostafa@gmail.com>
2023-11-14 13:41:30 -0800

    task 1

--- task_1.c
+++ task_1.c
@@ -8,36 +8,35 @@
 int print_int(va_list args, int *num)
 {
 	int n = va_arg(args, int);
-	int m, k = 1, i = 0, j, lastd = n % 10;
+	int m, k = 1, i = 0, j;
 
-	n = n / 10;
-	m = n;
+	if (n == INT_MIN)
+	{
+		write(1, "-2147483648", 11);
+		*num += 11;
+		return 0;
+	}
 
-	if (lastd < 0)
+	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
-		m = -m;
-		lastd = -lastd;
 		i++;
 	}
-	if (m > 0)
+	m = n;
+	while (m / 10 != 0)
 	{
-		while (m / 10 != 0)
-		{
-			m = m / 10;
-			k = k * 10;
-		}
-		while (k > 0)
-		{
-			j = n / k;
-			_putchar(j + '0');
-			n = n - (j * k);
-			k = k / 10;
-			i++;
-		}
+		m = m / 10;
+		k = k * 10;
+	}
+	while (k > 0)
+	{
+		j = n / k;
+		_putchar(j + '0');
+		n = n - (j * k);
+		k = k / 10;
+		i++;
 	}
 	*num += i;
-	_putchar(lastd + '0');
-	return (0);
+	return 0;
 }
