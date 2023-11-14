ffbda75bc801adbd2e3ed54d64a63d0891baa56d
Aymane <aymane.lamarti.8@gmail.com>
2023-11-14 02:00:36 +0000

    printf

--- task_0.c
+++ task_0.c
@@ -21,7 +21,7 @@ int _format(const char *format, va_list args, int *num)
 		if (*format != '%')
 		{
 			write(1, format, 1);
-			num++;
+			(*num)++;
 		}
 		else
 		{
@@ -32,18 +32,18 @@ int _format(const char *format, va_list args, int *num)
 			{
 				c = va_arg(args, int);
 				write(1, &c, 1);
-				num++;
+				(*num)++;
 			}
 			else if (*format == 's')
 			{
 				str = va_arg(args, char *);
 				write(1, str, strlen(str));
-				num += strlen(str) - 1;
+				(*num) += strlen(str) - 1;
 			}
 			else if (*format == '%')
 			{
 				write(1, "%", 1);
-				num++;
+				(*num)++;
 			}
 		format++;
 		}
